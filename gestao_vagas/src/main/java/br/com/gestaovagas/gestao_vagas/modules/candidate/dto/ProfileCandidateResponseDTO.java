a1e97cf4d991fa4349096c607220ac3c79e47afe
Crystian Lefundes <crystian.lf@gmail.com>
2025-05-05 22:23:19 -0300

    feature: Altera tipo de classe para record, mais adequado para um DTO

--- gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,30 +2,33 @@ package br.com.gestaovagas.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import br.com.gestaovagas.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProfileCandidateResponseDTO {
+public record ProfileCandidateResponseDTO (
 
     @Schema(description = "Identificador único do candidato", example = "f47ac10b-58cc-4372-a567-0e02b2c3d479")
-    private UUID id;
+    UUID id,
 
     @Schema(description = "Nome completo do candidato", example = "Gabriel André")
-    private String name;
+    String name,
 
     @Schema(description = "Nome de usuário do candidato", example = "gabriel123")
-    private String username;
+    String username,
 
     @Schema(description = "Endereço de e-mail do candidato", example = "gabriel@example.com")
-    private String email;
+    String email,
 
     @Schema(description = "Breve descrição do perfil do candidato", example = "Desenvolvedor Java com experiência em APIs REST.")
-    private String description;
+    String description
+) {
+    public ProfileCandidateResponseDTO(CandidateEntity candidate) {
+        this(
+                candidate.getId(),
+                candidate.getName(),
+                candidate.getUsername(),
+                candidate.getEmail(),
+                candidate.getDescription()
+        );
+    }
 }
