7191f3d6a7eed346c590747204db89293f3d39b6
Crystian Lefundes <crystian.lf@gmail.com>
2025-05-05 22:21:46 -0300

    feature: Adiciona mais metodos HTTP ao controller do candidato

--- gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/gestaovagas/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,8 +1,10 @@
 package br.com.gestaovagas.gestao_vagas.modules.candidate.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+import br.com.gestaovagas.gestao_vagas.modules.candidate.useCases.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
@@ -14,10 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import br.com.gestaovagas.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.gestaovagas.gestao_vagas.modules.candidate.entities.ApplyJobEntity;
 import br.com.gestaovagas.gestao_vagas.modules.candidate.entities.CandidateEntity;
-import br.com.gestaovagas.gestao_vagas.modules.candidate.useCases.ApplyJobCandidateUseCase;
-import br.com.gestaovagas.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
-import br.com.gestaovagas.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
-import br.com.gestaovagas.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
 import br.com.gestaovagas.gestao_vagas.modules.company.entities.JobEntity;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -47,6 +45,12 @@ public class CandidateController {
     @Autowired
     private ApplyJobCandidateUseCase applyJobCandidateUseCase;
 
+    @Autowired
+    private UpdateCandidateUseCase updateCandidateUseCase;
+
+    @Autowired
+    private DeleteCandidateUseCase deleteCandidateUseCase;
+
     @Operation(summary = "Criar novo candidato", description = """
             Registra um novo candidato com os seguintes campos:
             • name (2–100 caracteres)
@@ -58,14 +62,10 @@ public class CandidateController {
             Retorna o objeto `CandidateEntity` completo, já com `id` e `createdAt`.
             """)
     @PostMapping("/register")
-    public ResponseEntity<Object> create(
+    public ResponseEntity<ProfileCandidateResponseDTO> createCandidate(
             @Valid @RequestBody CandidateEntity candidateEntity) {
-        try {
             var result = createCandidateUseCase.execute(candidateEntity);
-            return ResponseEntity.ok(result);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+            return ResponseEntity.created(URI.create("/candidate/" + result.id())).body(result);
     }
 
     @Operation(summary = "Obter perfil do candidato autenticado", description = """
@@ -89,15 +89,10 @@ public class CandidateController {
     @SecurityRequirement(name = "jwt_auth")
     @PreAuthorize("hasRole('CANDIDATE')")
     @GetMapping("/me")
-    public ResponseEntity<Object> getProfile(HttpServletRequest request) {
+    public ResponseEntity<ProfileCandidateResponseDTO> getProfile(HttpServletRequest request) {
         var idCandidate = request.getAttribute("candidate_id");
-        try {
-            var profile = profileCandidateUseCase.execute(
-                    UUID.fromString(idCandidate.toString()));
-            return ResponseEntity.ok(profile);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        var profile = profileCandidateUseCase.execute(UUID.fromString(idCandidate.toString()));
+        return ResponseEntity.ok(profile);
     }
 
     @Operation(summary = "Listagem de vagas disponível para o candidato", description = "Retorna uma lista de vagas cujo título ou descrição contenham o termo informado em `filter`.")
@@ -128,15 +123,49 @@ public class CandidateController {
     @PostMapping("/job/apply")
     @PreAuthorize("hasRole('CANDIDATE')")
     @SecurityRequirement(name = "jwt_auth")
-    public ResponseEntity<Object> applyJob(HttpServletRequest request, @RequestBody UUID idJob) {
+    public ResponseEntity<ApplyJobEntity> applyJob(HttpServletRequest request, @RequestBody UUID idJob) {
         var idCandidate = request.getAttribute("candidate_id");
-
-        try {
-            var result = this.applyJobCandidateUseCase.execute(UUID.fromString(idCandidate.toString()), idJob);
-            return ResponseEntity.ok().body(result);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        var result = this.applyJobCandidateUseCase.execute(UUID.fromString(idCandidate.toString()), idJob);
+        return ResponseEntity.ok().body(result);
     }
 
+    @Operation(summary = "Atualizar perfil do candidato autenticado", description = """
+            Atualiza os dados do candidato logado:
+            • name, username, email, password
+            • description
+            """)
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Perfil do candidato atualizado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProfileCandidateResponseDTO.class), examples = @ExampleObject(name = "PerfilCandidatoExemplo", value = """
+                    {
+                      "id": "ad5eb131-adf6-4ef1-ac47-accbaa3dd474",
+                      "name": "Jake Snow",
+                      "username": "jake_snow",
+                      "email": "jakecompany@gmail.com",
+                      "description": "Sou techlead Back-End e busco oportunidades."
+                    }
+                    """))),
+            @ApiResponse(responseCode = "400", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "401", description = "Não autorizado")
+    })
+
+    @PutMapping("/update")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @SecurityRequirement(name = "jwt_auth")
+    public ResponseEntity<ProfileCandidateResponseDTO> updateCandidate(@Valid @RequestBody CandidateEntity candidate) {
+        return ResponseEntity.ok(this.updateCandidateUseCase.execute(candidate));
+    }
+
+    @Operation(summary = "Deletar um candidato", description = "Excluí um candidato informando seu UUID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Candidato deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "401", description = "Não autorizado")
+    })
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @SecurityRequirement(name = "jwt_auth")
+    public ResponseEntity<String> deleteCandidate(@PathVariable UUID id) {
+        deleteCandidateUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }
 }
